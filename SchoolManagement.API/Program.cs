8f405b891316f300bef6bc6a1e53063be45db40b
Mariana Caradec <maricaradec@gmail.com>
2025-04-23 18:26:39 -0300

    feat: added teacher and attendance's interface and service

--- SchoolManagement.API/Program.cs
+++ SchoolManagement.API/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using SchoolManagement.API.Data.Context;
+using SchoolManagement.API.Interfaces;
+using SchoolManagement.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ITeacherService, TeacherService>();
+builder.Services.AddScoped<IAttendanceService, AttendanceService>();
 
 builder.Services.AddDbContext<SchoolSysDBContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("SchoolSysDBConnection")));
