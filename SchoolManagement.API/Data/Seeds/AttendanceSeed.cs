6eae81b3936155282ca32c600d8c8008f1a62fc2
Mariana Caradec <maricaradec@gmail.com>
2025-04-23 18:24:37 -0300

    feat: added Attendance service and changed relationship with student and teacher

--- SchoolManagement.API/Data/Seeds/AttendanceSeed.cs
+++ SchoolManagement.API/Data/Seeds/AttendanceSeed.cs
@@ -13,19 +13,43 @@ namespace SchoolManagement.API.Data.Seeds
                 {
                     Id = 1,
                     Date = new DateOnly(2025, 4, 22),
-                    Present = true
+                    Present = true,
+                    StudentId = 1
                 },
                 new Attendance
                 {
                     Id = 2,
                     Date = new DateOnly(2025, 4, 21),
-                    Present = false
+                    Present = false,
+                    StudentId = 2
                 },
                 new Attendance
                 {
                     Id = 3,
                     Date = new DateOnly(2025, 4, 20),
-                    Present = true
+                    Present = true,
+                    StudentId = 3
+                },
+                new Attendance
+                {
+                    Id = 4,
+                    Date = new DateOnly(2025, 2, 20),
+                    Present = true,
+                    TeacherId = 1
+                },
+                new Attendance
+                {
+                    Id = 5,
+                    Date = new DateOnly(2025, 4, 14),
+                    Present = false,
+                    TeacherId = 2
+                },
+                new Attendance
+                {
+                    Id = 6,
+                    Date = new DateOnly(2025, 1, 9),
+                    Present = true,
+                    TeacherId = 3
                 }
             );
         }
--- SchoolManagement.API/Models/Attendance.cs
+++ SchoolManagement.API/Models/Attendance.cs
@@ -5,7 +5,9 @@
         public int Id { get; set; }
         public DateOnly Date { get; set; }
         public bool Present { get; set; }
-        public List<Student> Students { get; set; } = new(); //Muchos a muchos
-        public List<Teacher> Teachers { get; set; } = new(); //Muchos a muchos
+        public int? StudentId { get; set; }
+        public Student Student { get; set; } //Muchos a uno
+        public int? TeacherId { get; set; }
+        public Teacher Teacher { get; set; } //Muchos a uno
     }
 }
