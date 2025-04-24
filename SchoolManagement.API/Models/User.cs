d4f9214ccb7382e29ce231fd5600221e0c91fa22
Mariana Caradec <maricaradec@gmail.com>
2025-04-23 22:40:56 -0300

    feat: created User model for better business logic

---
+++ SchoolManagement.API/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace SchoolManagement.API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; } // "Admin", "Teacher", "Student"
+        public Teacher? Teacher { get; set; } //Uno a uno opcional
+        public Student? Student { get; set; } //Uno a uno opcional
+    }
+}
