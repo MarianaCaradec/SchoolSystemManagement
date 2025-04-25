456501383f7942232665892911336c7f49c66672
Mariana Caradec <maricaradec@gmail.com>
2025-04-25 14:46:08 -0300

    feat: added class controller

---
+++ SchoolManagement.API/Controllers/ClassController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using SchoolManagement.API.Data.Seeds;
+using SchoolManagement.API.Interfaces;
+using SchoolManagement.API.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SchoolManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClassController(IClassService classService) : ControllerBase
+    {
+
+        private readonly IClassService _classService = classService;
+
+        // GET: api/<ClassController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Class>>> GetAllClasses()
+        {
+            IEnumerable<Class> classes = await _classService.GetClassesAsync();
+
+            if (classes == null || !classes.Any()) return NoContent();
+
+            return Ok(classes);
+        }
+
+        // GET api/<ClassController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Class>> GetClass(int id)
+        {
+            Class classById = await _classService.GetClassByIdAsync(id);
+
+            return Ok(classById);
+        }
+
+        // POST api/<ClassController>
+        [HttpPost]
+        public async Task<ActionResult<Class>> PostClass(Class classToBeCreated)
+        {
+            Class createdClass = await _classService.CreateClassAsync(classToBeCreated);
+
+            return CreatedAtAction("GetClass", new { id = createdClass.Id }, createdClass);
+        }
+
+        // PUT api/<ClassController>/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Class>> PutClass(int id, Class classToBeUpdated)
+        {
+            Class updatedClass = await _classService.UpdateClassAsync(id, classToBeUpdated);
+            
+            return Ok(updatedClass);
+        }
+
+        // DELETE api/<ClassController>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<bool>> DeleteClass(int id)
+        {
+            await _classService.DeleteClassAsync(id);
+
+            return NoContent();
+        }
+    }
+}
