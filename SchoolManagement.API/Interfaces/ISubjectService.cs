e2c290c1601d717f186b806965e25feeaf6bd2d1
Mariana Caradec <maricaradec@gmail.com>
2025-04-28 19:12:00 -0300

    feat: added checking user role to allow them to do certain actions

--- SchoolManagement.API/Interfaces/ISubjectService.cs
+++ SchoolManagement.API/Interfaces/ISubjectService.cs
@@ -4,10 +4,10 @@ namespace SchoolManagement.API.Interfaces
 {
     public interface ISubjectService
     {
-        Task<IEnumerable<Subject>> GetSubjectsAsync();
-        Task<Subject> GetSubjectByIdAsync(int id);
-        Task<Subject> CreateSubjectAsync(Subject subjectToBeCreated);
-        Task<Subject> UpdateSubjectAsync(int id, Subject subjectToBeUpdated);
-        Task<bool> DeleteSubjectAsync(int id);
+        Task<IEnumerable<Subject>> GetSubjectsAsync(int userId);
+        Task<Subject> GetSubjectByIdAsync(int id, int userId);
+        Task<Subject> CreateSubjectAsync(Subject subjectToBeCreated, int userId);
+        Task<Subject> UpdateSubjectAsync(int id, Subject subjectToBeUpdated, int userId);
+        Task<bool> DeleteSubjectAsync(int id, int userId);
     }
 }
