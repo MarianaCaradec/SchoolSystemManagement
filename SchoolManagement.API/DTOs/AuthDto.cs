48775ac8081c00e18f87e72227a76dfc38ce3cbf
Mariana Caradec <maricaradec@gmail.com>
2025-05-06 18:57:36 -0300

    feat: added needed DTOs for AuthService

---
+++ SchoolManagement.API/DTOs/AuthDto.cs
@@ -0,0 +1,11 @@
+﻿using SchoolManagement.API.Models;
+
+namespace SchoolManagement.API.DTOs
+{
+    public class AuthDto
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public User.UserRole Role { get; set; }
+    }
+}
