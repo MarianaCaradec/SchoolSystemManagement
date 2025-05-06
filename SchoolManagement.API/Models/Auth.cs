30683f7e4d04763d466951294e7f326a4e723d74
Mariana Caradec <maricaradec@gmail.com>
2025-05-06 18:56:50 -0300

    feat: created necessary model for authentication

---
+++ SchoolManagement.API/Models/Auth.cs
@@ -0,0 +1,15 @@
+﻿using static SchoolManagement.API.Models.User;
+
+namespace SchoolManagement.API.Models
+{
+    public class Auth
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public long? MobileNumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public UserRole Role { get; set; }
+    }
+}
