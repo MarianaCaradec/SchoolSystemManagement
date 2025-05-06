4fa25580c458c3c3125629232e932dd462b8f423
Mariana Caradec <maricaradec@gmail.com>
2025-05-06 19:03:05 -0300

    feat: added request models for AuthController

---
+++ SchoolManagement.API/Models/AuthReq.cs
@@ -0,0 +1,9 @@
+﻿namespace SchoolManagement.API.Models
+{
+    public class AuthReq
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int UserId { get; set; }
+    }
+}
