01f90e7f0977e6627c25edcb5944e1d7115bcd96
Mariana Caradec <maricaradec@gmail.com>
2025-05-07 14:11:43 -0300

    feat: added forgotten relationship with teacher and student entities

--- SchoolManagement.API/DTOs/UserDto.cs
+++ SchoolManagement.API/DTOs/UserDto.cs
@@ -7,5 +7,7 @@ namespace SchoolManagement.API.DTOs
         public int Id { get; set; }
         public string Email { get; set; }
         public User.UserRole Role { get; set; }
+        public Teacher? Teacher { get; set; }
+        public Student? Student { get; set; }
     }
 }
