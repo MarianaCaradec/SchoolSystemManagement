014b413e819fa1b46bf82b02f5debc1b5308f806
Mariana Caradec <maricaradec@gmail.com>
2025-05-08 19:56:21 -0300

    fix: added nullable userId to allow creating an user with student role from start

--- SchoolManagement.API/Interfaces/IUserService.cs
+++ SchoolManagement.API/Interfaces/IUserService.cs
@@ -9,7 +9,7 @@ namespace SchoolManagement.API.Interfaces
         public Task<IEnumerable<UserDto>> GetUsersAsync(int userId);
         public Task<UserDto> GetUserByEmailAsync(string email, int userId);
         public Task<UserRole> GetUserRole(int id);
-        public Task<UserDto> CreateUserAsync(UserInputDto userToBeCreated, int userId);
+        public Task<UserDto> CreateUserAsync(UserInputDto userToBeCreated, int? userId);
         public Task<UserDto> UpdateUserAsync(int id, UserInputDto userToBeUpdated, int userId);
         public Task<bool> DeleteUserAsync(int id, int userId);
     }
