0a064461c1a6f01d8897a53c98085773bca0d2dc
Mariana Caradec <maricaradec@gmail.com>
2025-05-08 19:01:36 -0300

    feat: added the relationships needed to show User with Teacher or Student

---
+++ SchoolManagement.API/DTOs/StudentDto.cs
@@ -0,0 +1,12 @@
+﻿using SchoolManagement.API.Models;
+
+namespace SchoolManagement.API.DTOs
+{
+    public class StudentDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public ClassDto Class { get; set; }
+    }
+}
