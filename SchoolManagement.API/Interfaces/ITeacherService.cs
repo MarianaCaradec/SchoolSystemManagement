606865ba01b5ccdd2fc7635311ca898a65c1eb51
Mariana Caradec <maricaradec@gmail.com>
2025-05-09 13:56:37 -0300

    feat: added needed DTOs

--- SchoolManagement.API/Interfaces/ITeacherService.cs
+++ SchoolManagement.API/Interfaces/ITeacherService.cs
@@ -1,12 +1,13 @@
-﻿using SchoolManagement.API.Models;
+﻿using SchoolManagement.API.DTOs;
+using SchoolManagement.API.Models;
 
 namespace SchoolManagement.API.Interfaces
 {
     public interface ITeacherService
     {
-        Task<IEnumerable<Teacher>> GetTeachersAsync(int userId);
-        Task<Teacher> GetTeacherByIdAsync(int id, int userId);
-        Task<Teacher> CreateTeacherAsync(Teacher teacherToBeCreated, int userId);
+        Task<IEnumerable<TeacherDto>> GetTeachersAsync(int userId);
+        Task<TeacherDto> GetTeacherByIdAsync(int id, int userId);
+        Task<TeacherInputDto> CreateTeacherAsync(TeacherInputDto teacherToBeCreated, int userId);
         Task<Teacher> UpdateTeacherAsync(int id, Teacher teacherToBeUpdated, int userId);
         Task<bool> DeleteTeacherAsync(int id, int userId);
     }
