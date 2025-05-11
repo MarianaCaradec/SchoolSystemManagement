bfa56e6fcfe501a696baf8647a91ce4cb149989e
Mariana Caradec <maricaradec@gmail.com>
2025-05-11 16:10:40 -0300

    feat: created needed DTOs for Subject's returns

---
+++ SchoolManagement.API/DTOs/SubjectResponseDto.cs
@@ -0,0 +1,12 @@
+﻿using SchoolManagement.API.Models;
+
+namespace SchoolManagement.API.DTOs
+{
+    public class SubjectResponseDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public List<TeacherResponseDto> Teachers { get; set; } = new(); //Muchos a muchos
+        public List<Grade> Grades { get; set; } = new();
+    }
+}
---
+++ SchoolManagement.API/DTOs/TeacherResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SchoolManagement.API.DTOs
+{
+    public class TeacherResponseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public long MobileNumber { get; set; }
+    }
+}
