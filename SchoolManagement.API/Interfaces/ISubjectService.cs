1c067f801b178611d24b8435332d3a9d78921e27
Mariana Caradec <maricaradec@gmail.com>
2025-05-11 16:09:57 -0300

    feat: added needed DTO in GET method

--- SchoolManagement.API/Interfaces/ISubjectService.cs
+++ SchoolManagement.API/Interfaces/ISubjectService.cs
@@ -1,10 +1,11 @@
-﻿using SchoolManagement.API.Models;
+﻿using SchoolManagement.API.DTOs;
+using SchoolManagement.API.Models;
 
 namespace SchoolManagement.API.Interfaces
 {
     public interface ISubjectService
     {
-        Task<IEnumerable<Subject>> GetSubjectsAsync(int userId);
+        Task<IEnumerable<SubjectResponseDto>> GetSubjectsAsync(int userId);
         Task<Subject> GetSubjectByIdAsync(int id, int userId);
         Task<Subject> CreateSubjectAsync(Subject subjectToBeCreated, int userId);
         Task<Subject> UpdateSubjectAsync(int id, Subject subjectToBeUpdated, int userId);
