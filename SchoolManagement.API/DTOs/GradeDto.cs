d85a11c43ee54345b3b407e559df58d5235fcd9b
Mariana Caradec <maricaradec@gmail.com>
2025-05-12 20:11:32 -0300

    feat: created needed DTOs for POST and PUT methods and added needed atributes for teacher

---
+++ SchoolManagement.API/DTOs/GradeDto.cs
@@ -0,0 +1,13 @@
+﻿using SchoolManagement.API.Models;
+
+namespace SchoolManagement.API.DTOs
+{
+    public class GradeDto
+    {
+        public int Id { get; set; }
+        public int Value { get; set; }
+        public DateOnly Date { get; set; }
+        public int StudentId { get; set; }
+        public int SubjectId { get; set; }
+    }
+}
---
+++ SchoolManagement.API/DTOs/SubjectInputDto.cs
@@ -0,0 +1,12 @@
+﻿using SchoolManagement.API.Models;
+
+namespace SchoolManagement.API.DTOs
+{
+    public class SubjectInputDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public List<TeacherResponseDto>? Teachers { get; set; } = new();
+        public List<GradeDto>? Grades { get; set; } = new();
+    }
+}
