bd1303291ad48634ec35844948cc0d1a410e2a0d
Mariana Caradec <maricaradec@gmail.com>
2025-05-12 20:11:32 -0300

    feat: created needed DTOs for POST and PUT methods and added needed atributes for teacher

--- SchoolManagement.API/DTOs/TeacherResponseDto.cs
+++ SchoolManagement.API/DTOs/TeacherResponseDto.cs
@@ -6,5 +6,7 @@
         public string Name { get; set; }
         public string Surname { get; set; }
         public long MobileNumber { get; set; }
+        public string Address { get; set; }
+        public int UserId { get; set; }
     }
 }
