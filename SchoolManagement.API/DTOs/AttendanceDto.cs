b159fda35f951adef7340ff69fd34d37c35272ee
Mariana Caradec <maricaradec@gmail.com>
2025-05-13 18:28:49 -0300

    chore: removed unneeded relationships

--- SchoolManagement.API/DTOs/AttendanceDto.cs
+++ SchoolManagement.API/DTOs/AttendanceDto.cs
@@ -8,8 +8,6 @@ namespace SchoolManagement.API.DTOs
         public DateOnly Date { get; set; }
         public bool Present { get; set; }
         public int? StudentId { get; set; }
-        public Student Student { get; set; } //Muchos a uno
         public int? TeacherId { get; set; }
-        public Teacher Teacher { get; set; } //Muchos a uno
     }
 }
