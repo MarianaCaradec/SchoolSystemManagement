f2b8c69634225f3aa07d373e9fec4ee3f676d130
Mariana Caradec <maricaradec@gmail.com>
2025-05-13 18:25:52 -0300

    feat: created DTO for GET and GETById methods

---
+++ SchoolManagement.API/DTOs/StudentResponseDto.cs
@@ -0,0 +1,16 @@
+﻿namespace SchoolManagement.API.DTOs
+{
+    public class StudentResponseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public string Address { get; set; }
+        public long MobileNumber { get; set; }
+        public AuthDto? EmailRole { get; set; }
+        public ClassStudentDto? Class { get; set; }
+        public List<AttendanceDto>? Attendances { get; set; } = new();
+        public List<GradeDto>? Grades { get; set; } = new();
+    }
+}
