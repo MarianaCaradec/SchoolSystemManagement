7735e9ef9a31aea434ddf57289e520615aa9a8a0
Mariana Caradec <maricaradec@gmail.com>
2025-05-13 18:25:25 -0300

    fix: added neede DTO

--- SchoolManagement.API/Interfaces/IStudentService.cs
+++ SchoolManagement.API/Interfaces/IStudentService.cs
@@ -1,11 +1,12 @@
-﻿using SchoolManagement.API.Models;
+﻿using SchoolManagement.API.DTOs;
+using SchoolManagement.API.Models;
 
 namespace SchoolManagement.API.Interfaces
 {
     public interface IStudentService
     {
-        Task<IEnumerable<Student>> GetStudentsAsync(int userId);
-        Task<Student> GetStudentByIdAsync(int id, int userId);
+        Task<IEnumerable<StudentResponseDto>> GetStudentsAsync(int userId);
+        Task<StudentResponseDto> GetStudentByIdAsync(int id, int userId);
         Task<Student> CreateStudentAsync(Student studentToBeCreated, int userId);
         Task<Student> UpdateStudentAsync(int id, Student studentToBeUpdated, int userId);
         Task<bool> DeleteStudentAsync(int id, int userId);
