885348f61d46a288484f5ff5cccc4ed8146003ff
Mariana Caradec <maricaradec@gmail.com>
2025-05-14 08:52:22 -0300

    feat: created DTO for CREATE method

---
+++ SchoolManagement.API/DTOs/StudentInputDto.cs
@@ -0,0 +1,16 @@
+﻿using SchoolManagement.API.Models;
+
+namespace SchoolManagement.API.DTOs
+{
+    public class StudentInputDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public string Address { get; set; }
+        public long MobileNumber { get; set; }
+        public int UserId { get; set; }
+        public int ClassId { get; set; }
+    }
+}
