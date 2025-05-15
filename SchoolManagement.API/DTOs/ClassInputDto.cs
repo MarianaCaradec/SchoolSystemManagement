a1ced883c013cf566d38caafb43929add346cc1d
Mariana Caradec <maricaradec@gmail.com>
2025-05-15 19:18:14 -0300

    feat: created needed DTO for POST method

---
+++ SchoolManagement.API/DTOs/ClassInputDto.cs
@@ -0,0 +1,12 @@
+﻿namespace SchoolManagement.API.DTOs
+{
+    public class ClassInputDto
+    {
+        public int Id { get; set; }
+        public string Course { get; set; }
+        public string Divition { get; set; }
+        public int Capacity { get; set; }
+        public int TeacherId { get; set; }
+        public int StudentId { get; set; }
+    }
+}
