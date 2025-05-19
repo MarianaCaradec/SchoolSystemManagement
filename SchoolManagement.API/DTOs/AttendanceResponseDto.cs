17ee879ec299b9b4c53349d06401c80cb1914ea9
Mariana Caradec <maricaradec@gmail.com>
2025-05-19 11:09:23 -0300

    feat: added new DTO for GetById method

---
+++ SchoolManagement.API/DTOs/AttendanceResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SchoolManagement.API.DTOs
+{
+    public class AttendanceResponseDto
+    {
+        public int Id { get; set; }
+        public DateOnly Date { get; set; }
+        public bool Present { get; set; }
+        public StudentInputDto? Student { get; set; }
+        public TeacherInputDto? Teacher { get; set; }
+    }
+}
