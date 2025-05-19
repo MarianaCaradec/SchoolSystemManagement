0139ca2b33d896e59b676f2fbf6f173f81b8b4c8
Mariana Caradec <maricaradec@gmail.com>
2025-05-19 11:10:30 -0300

    fix: added needed DTO to GETById method

--- SchoolManagement.API/Interfaces/IAttendanceService.cs
+++ SchoolManagement.API/Interfaces/IAttendanceService.cs
@@ -6,7 +6,7 @@ namespace SchoolManagement.API.Interfaces
     public interface IAttendanceService
     {
         Task<IEnumerable<AttendanceDto>> GetAttendancesAsync(int userId);
-        Task<Attendance> GetAttendanceByIdAsync(int id, int userId);
+        Task<AttendanceResponseDto> GetAttendanceByIdAsync(int id, int userId);
         Task<Attendance> CreateAttendanceAsync(Attendance attendanceToBeCreated, int? studentId, int? teacherId, int userId);
         Task<Attendance> UpdateAttendanceAsync(int id, Attendance attendanceToBeUpdated, int? studentId, int? teacherId, int userId);
         Task<bool> DeleteAttendanceAsync(int id, int userId);
